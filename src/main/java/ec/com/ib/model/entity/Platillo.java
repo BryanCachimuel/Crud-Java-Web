c1e0cd557b6a871fe63a7237985105a9f77c44d0
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-12-19 23:02:02 -0500

    Creando la clase Platillo la cual implementa de un tipo serialize y la misma contiene los atributos de la entidad platillo definida en la base de datos

---
+++ src/main/java/ec/com/ib/model/entity/Platillo.java
@@ -0,0 +1,29 @@
+package ec.com.ib.model.entity;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "platillo")
+public class Platillo implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_platillo")
+    private Integer IdPlatillos;
+    
+    @Column(name = "nombre")
+    private String nombre;
+    
+    @Column(name = "descripcion")
+    private String descripcion;
+    
+    @Column(name = "precio")
+    private double precio;
+    
+}
